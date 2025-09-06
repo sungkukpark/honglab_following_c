799e93a21ccc040fb703efdef553b541ed6b0fc6
Sungkuk Park <sungkukshawnpark@gmail.com>
2025-09-06 16:11:43 +0200

    add initial commit

---
+++ MyFirstProject/MyFirstProject.cpp
@@ -0,0 +1,21 @@
+﻿// MyFirstProject.cpp : 이 파일은 'main' 함수를 포함하고 있다. 이 프로그램은 해당 'main' 함수에서 시작하고 'main' 함수에서 끝난다.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 프로그램 실행: "Ctrl + F5"를 누르거나 "Debug > Start Without Debugging" 메뉴를 선택한다
+// 프로그램 디버깅: "F5"를 누르거나 "Debug > Start Debugging" 메뉴를 선택한다
+
+// Visual Studio를 시작하는 팁들: 
+//   1. 솔루션 탐색기(Solution Explorer) 창을 통해 파일을 추가하거나 관리한다
+//   2. 팀 탐색기(Team Explorer) 창을 통해 형상 관리(source control) 시스템에 연결한다
+//   3. 출력(Output) 창을 통해 빌드 관련 출력이나 기타 메시지를 확인한다
+//   4. 에러 리스트(Error List) 창을 통해 에러들을 확인한다
+//   5. 프로젝트(Project) > 새 아이템 추가(Add New Item)를 통해 새 코드 파일을 생성하고,
+//      프로젝트(Project) > 기존의 아이템 추가(Add Existing Item)을 통해 새 기존 코드 파일을 프로젝트에 추가한다
+//   6. 나중에 이 프로젝트를 새로 열고 싶으면 파일(File) > 열기(Open) > 프로젝트(Project)로 가서 해당 ".sln" 파일을 선택한다
