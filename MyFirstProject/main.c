05d18312e3e0f97664058cb3b3add2ee71d69f17
Sungkuk Park <sungkukshawnpark@gmail.com>
2025-09-12 00:25:14 +0200

    implement my first strlen function

--- MyFirstProject/main.c
+++ MyFirstProject/main.c
@@ -1,7 +1,24 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int32_t main(void) {
-  // Print "hello, world"
-  printf("hello, world");
+size_t _strlen(char *one)
+{
+    size_t length = 0;
+    for (size_t i = 0; i < sizeof(one); ++i)
+    {
+        if (one[i] == '\0')
+        {
+            return length;
+        }
+
+        ++length;
+    }
+}
+
+int32_t main(void)
+{
+    char one[2] = "1";
+    printf(one);
+
+    printf("%zu", _strlen(one));
 }
