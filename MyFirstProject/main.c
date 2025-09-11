d813f509b93ccd6f3aa1d23700506d310519d512
Sungkuk Park <sungkukshawnpark@gmail.com>
2025-09-12 01:11:24 +0200

    add number array

--- MyFirstProject/main.c
+++ MyFirstProject/main.c
@@ -13,6 +13,17 @@ size_t _strlen(char *str)
 
 int32_t main(void)
 {
+    int num = 1;
+    printf("%d (1)\n", num);
+
+    int num_array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    size_t size_of_num_array = sizeof(num_array) / sizeof(int);
+    for (size_t i = 0; i < size_of_num_array; ++i)
+    {
+        printf("%d ", num_array[i]);
+    }
+    printf("(%zu) \n", size_of_num_array);
+
     char one[2] = "1";
     printf("%s (%zu)\n", one, _strlen(one));
 
